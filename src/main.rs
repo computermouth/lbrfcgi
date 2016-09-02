c5d52ab9542556ae68bc359ae6a00d4b8c6719f1
computermouth <computermouth@crunchbangplusplus.org>
2016-09-02 10:57:52 -0700

    output changes

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,21 @@ fn main() {
     fcgi::initialize_fcgi();
     let mut request: DefaultRequest = Request::new().unwrap();
     while request.accept() {
-        println!("request uri    {:?}", request.get_param("REQUEST_URI"));
-        println!("document root  {:?}", request.get_param("DOCUMENT_ROOT"));
-        println!("script name    {:?}", request.get_param("SCRIPT_NAME"));
-        println!("request method {:?}", request.get_param("REQUEST_METHOD"));
-        println!("remote user    {:?}", request.get_param("REMOTE_USER"));
+        let uri = request.get_param("REQUEST_URI");
+        println!("request uri    {:?}", uri);
+        
+        let root = request.get_param("DOCUMENT_ROOT");
+        println!("document root  {:?}", root);
+        
+        let script = request.get_param("SCRIPT_NAME");
+        println!("script name    {:?}", script);
+        
+        let method = request.get_param("REQUEST_METHOD");
+        println!("request method {:?}", method);
+        
+        let user = request.get_param("REMOTE_USER");
+        println!("remote user    {:?}", user);
+        
         let received = request.readall();
         println!("Received (size={})", received.len());
         if received.len() > 0 {
@@ -20,8 +30,10 @@ fn main() {
             println!("{}", received);
             println!("8<------------------");
         }
+        
         let body = "Content-type: text/html\r\n\r\n<header><title>Hello World</title></header>\r\n<body> <h1>Hello World!</h1>  </body>";
-        let byte_count = request.write(body);
+        
+        let byte_count = request.write(c_type);
         request.error("Test error!");
         println!("number of bytes written {}", byte_count);
         request.flush(fcgi::StreamType::OutStream);
